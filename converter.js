7dfca152083b71eaafaa553384011f148f813da6
Ethan <ethangeyer1122@gmail.com>
2021-05-16 21:01:03 -0400

    Added Pet Converter

---
+++ converter.js
@@ -0,0 +1,43 @@
+var name = new URLSearchParams(window.location.search)
+var changeFirName = name.split('&')[0];
+var firstPet = changeFirName.replace('firpet=','');
+var changeSecName = name.split("&").pop();
+var secondPet = changeSecName.replace('secpet=','');
+
+
+var humanAge = 79;
+var dogAge = 11;
+var catAge = 15;
+var fishAge = 3;
+var hamsterAge = 3;
+
+var isNumber = true;
+var animalType;
+
+petDictionary = {
+    'dog': dogAge,
+    'cat': catAge,
+    'hamster': hamsterAge,
+    'fish': fishAge,
+    'human': humanAge,
+}
+
+var firstParam = petDictionary[firstPet];
+var secondParam = petDictionary[secondPet];
+
+var answer = firstParam / secondParam;
+
+if(answer >= 1) {
+    answer = Math.round(answer);
+}
+else {
+    answer = answer.toFixed(2);
+}
+
+if(isNaN(firstParam) || isNaN(secondParam)) {
+    isNumber = false;
+}
+
+if(isNumber) {
+    document.getElementById('animalYears').innerHTML = "1 " + firstPet + " year is equivalent to approximately " + answer + " " + secondPet + " years";
+}
